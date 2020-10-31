554f0ca9448c006f5f3221e968acb0fe49038e91
kwiel <kwiel@poczta.onet.pl>
2020-10-31 11:10:00 +0100

    jj

---
+++ src/views/root/Root.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from 'components/Button/Button';
+
+const Root = () => (
+  <div>
+    <h1>hello Kris</h1>
+    <Button>o jo joj </Button>
+  </div>
+);
+export default Root;
